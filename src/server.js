c32813b547a00ce65cf9716e9884aa5ca3f2a98b
Pedro Costa <pedro_babysco@outlook.com>
2020-08-08 16:47:52 -0300

    Atualizaçoẽs e servidor

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const server = express()
+
+server.use(express.static("public"))
+
+.get("/", (req, res) => {
+    return res.sendfile(__dirname + "/views/index.html");
+})
+.get("/study", (req, res) => {
+    return res.sendfile(__dirname + "/views/study.html");
+})
+.get("/give-classes", (req, res) => {
+    return res.sendfile(__dirname + "/views/give-classes.html");
+})
+
+.listen(5500);
